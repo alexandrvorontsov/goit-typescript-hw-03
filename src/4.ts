3f3eb07e5933510be2f350920e4e823e5d1aa12b
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2023-11-18 18:10:28 +0200

    add

---
+++ src/4.ts
@@ -0,0 +1,54 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  private tenants: Person[];
+
+  constructor(protected key: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(key: Key): void {
+    this.door = key.getSignature() === this.key.getSignature();
+  }
+}
+
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
